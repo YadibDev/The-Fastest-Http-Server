6047a75cb7feaa721925dfa8bac3a6b584179424
mossaab-jaouchi <mosabejaouchi3@gmail.com>
2026-02-18 15:28:05 +0000

    test configue

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "ParseConfigFile/ConfigFile/ParseConfigueFile.hpp"
+
+enum TokenType {
+    TOKEN_NULL,
+    TOKEN_WORD,
+    TOKEN_LBRACE,
+    TOKEN_RBRACE,
+    TOKEN_SEMICOLON,
+    TOKEN_FASSILA,
+    TOKEN_JOUJNO9ATE,
+    TOKEN_EOF
+};
+
+int main()
+{
+    std::string Data;
+    int fd = open("config.conf", O_RDONLY);
+    ssize_t Size = 1024;
+
+    HelperFunctions::ReadData(fd, Data, Size);
+
+    LexerConfig<TokenType> cfg(TokenType::TOKEN_WORD, TokenType::TOKEN_EOF, TokenType::TOKEN_NULL);
+
+    GenericLexer<TokenType> Lexer(Data, cfg);
+
+    std::vector< Token<TokenType> > Tokens = Lexer.tokenize();
+
+    clsParse Parse(Tokens, TokenType::TOKEN_EOF);
+    clsParseConfigueFile configFile(Parse);
+
+
+    
+    return 0;
+}
