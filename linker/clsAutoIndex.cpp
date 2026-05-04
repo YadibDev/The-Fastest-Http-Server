a176e04afc468a35a950fe1ed64a495e667a1125
yassine-adib <funnyforyou5@gmail.com>
2026-05-04 13:45:44 +0000

    done auto index 99%

--- linker/clsAutoIndex.cpp
+++ linker/clsAutoIndex.cpp
@@ -48,7 +48,7 @@ void clsAutoIndex::initializeAutoIndex(const char *physicalDir, const char *dir,
 flowAutoIndex clsAutoIndex::_canPush(short nameSize, short availableSize)
 {
     short result;
-    result = (dirSize + nameSize) * 3 + nameSize + 14 + 3 + 10 + 5 * 2 + 10; // is hardcode NO!!!!
+    result = (dirSize + nameSize) * 3 + nameSize + 14 + 3 + 10 + 5 * 2 + 9; // is hardcode NO!!!! IMPROVE IT
     if (result > availableSize)
         return I_CANT_PUSH;
     else
@@ -169,7 +169,6 @@ void clsAutoIndex::_addSizeChunk(short pushBytes, bool lastChunk)
     memset(ptrSize, '0', size);
     memcpy(&ptrSize[size], hexa.c_str(), hexa.size());
     memcpy(&ptrSize[4], "\r\n", 2);
-    std::cout << ptrSize << std::endl;
     ptrSize = NULL;
 }
 
@@ -222,6 +221,8 @@ flowAutoIndex clsAutoIndex::insertAutoDirective(char *buffer, short &start, shor
         flowEnum = CAN_PUSH;
         while ((ptrDir = readdir(streamDir)) != NULL && flowEnum == CAN_PUSH)
         {
+            if (strcmp(ptrDir->d_name, ".") == 0 || strcmp(ptrDir->d_name, "..") == 0)
+                continue;
             flowEnum = _insertRow(buffer, start, pushbytes, limitSize);
         }
         if (ptrDir == NULL)
@@ -250,146 +251,3 @@ flowAutoIndex clsAutoIndex::insertAutoDirective(char *buffer, short &start, shor
     return flowEnum;
 }
 
-// int addDataToChar(char *buffer, const char *src, short &start, short len, bool encode = false)
-// {
-//     if (encode)
-//     {
-//         short oldStart = start;
-//         for (int i = 0; i < len; i++)
-//         {
-//             if (is_unreserved(src[i]))
-//                 buffer[start++] = src[i];
-//             else
-//             {
-//                 buffer[start++] = '%';
-//                 int temp = (int)src[i];
-//                 if (temp < 16)
-//                     buffer[start++] = '0';
-//                 string hexa = HelperFunctions::Convert_Hex("0123456789abcdef", temp);
-//                 memcpy(&buffer[start], hexa.c_str(), hexa.size());
-//                 start += hexa.size();
-//             }
-//         }
-//         return start - oldStart;
-//     }
-//     else
-//     {
-//         memcpy(&buffer[start], src, len);
-//         start += len;
-//     }
-//     return len;
-// }
-
-// short pushLink(char *buffer, short &start, char *dir, char *file, unsigned char FILE_TYPE)
-// {
-//     int result = 0;
-//     int fileLen = strlen(file);
-//     int dirLen = strlen(dir);
-
-//     result += addDataToChar(buffer, "<a href='", start, 9);
-//     result += addDataToChar(buffer, dir, start, dirLen, true);
-//     result += addDataToChar(buffer, file, start, fileLen, true);
-//     if (FILE_TYPE == DT_DIR)
-//         result += addDataToChar(buffer, "/", start, 1);
-//     result += addDataToChar(buffer, "'>", start, 2);
-//     result += addDataToChar(buffer, file, start, fileLen);
-//     result += addDataToChar(buffer, "</a>", start, 4);
-//     return result;
-// }
-
-// short pushTypeFile(char *buffer, short &start, unsigned char FILE_TYPE)
-// {
-//     char *ptr;
-//     if (FILE_TYPE == DT_REG)
-//         ptr = "FILE";
-//     else if (FILE_TYPE == DT_DIR)
-//         ptr = "DIRECTORY";
-//     else
-//         ptr = "UNKNONW";
-
-//     return addDataToChar(buffer, ptr, start, strlen(ptr));
-// }
-
-// short pushNewRow(char *buffer, short &start, char *dir, char *file, unsigned char FILE_TYPE)
-// {
-//     int result = 0;
-
-//     result += addDataToChar(buffer, preffRow.c_str(), start, preffRow.size());
-
-//     result += addDataToChar(buffer, preffCol.c_str(), start, preffCol.size());
-//     result += pushLink(buffer, start, dir, file, FILE_TYPE);
-//     result += addDataToChar(buffer, suffCol.c_str(), start, suffCol.size());
-
-//     result += addDataToChar(buffer, preffCol.c_str(), start, preffCol.size());
-//     result += pushTypeFile(buffer, start, FILE_TYPE);
-//     result += addDataToChar(buffer, suffCol.c_str(), start, suffCol.size());
-
-//     result += addDataToChar(buffer, suffRow.c_str(), start, suffRow.size());
-
-//     return result;
-// }
-
-// flowAutoIndex insertAutoDirective(char *buffer, short &start, short limitSize, char *dir)
-// {
-//     static DIR *streamDir = opendir(dir);
-//     static dirent *ptrDir = NULL;
-//     char *ptrSize = NULL;
-//     short pushbytes = 0;
-
-//     if (flowEnum == start)
-//     {
-//         ptrSize = &buffer[start];
-//         start += 6;
-//         pushbytes += pushHeaderAutoIndex(buffer, start, dir);
-//         flowEnum = TRY;
-//     }
-//     else if (flowEnum == I_CANT_PUSH)
-//     {
-
-//         exit(flowEnum == CAN_PUSH);
-//     }
-
-//     if (flowEnum == TRY)
-//     {
-//         flowEnum = CAN_PUSH;
-//         while ((ptrDir = readdir(streamDir)) != NULL && flowEnum == CAN_PUSH)
-//         {
-//             flowEnum = canPush(strlen(dir), strlen(ptrDir->d_name), limitSize - pushbytes - 6);
-//             if (flowEnum == I_CANT_PUSH)
-//                 break;
-//             pushbytes += pushNewRow(buffer, start, dir, ptrDir->d_name, ptrDir->d_type);
-//         }
-//         if (ptrDir == NULL)
-//             flowEnum = END_TAG;
-//     }
-
-//     if (pushbytes > 0)
-//     {
-//         // update offset buffer
-//     }
-//     return flowEnum;
-// }
-
-int main()
-{
-    clsAutoIndex autoIndex;
-    std::ofstream output("output.html");
-
-    char buffer[16000];
-    short start = 0;
-    autoIndex.initializeAutoIndex("/goinfre/yadib/The-Fastest-Http-Server/linker", "The-Fastest-Http-Server/linker", 45, 30);
-    flowAutoIndex flow;
-    while ((flow = autoIndex.insertAutoDirective(buffer, start, 500)) < DONE_AUTO_INDEX)
-    {
-        buffer[start] = '\0';
-        std::cout << start << std::endl;
-        std::cout << buffer << std::endl;
-        start = 0;
-        output << buffer;
-    }
-    buffer[start] = '\0';
-    output << buffer;
-
-    if (flow == DONE_AUTO_INDEX)
-        std::cout << "DONE_AUTO_INDEX" << std::endl;
-}
