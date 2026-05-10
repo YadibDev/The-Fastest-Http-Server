53c8e9faf6fbd5fd3337312b0d5bb7a907b3dc58
Achraf Hamdaoui <achamdao@e1r5p13.1337.ma>
2026-05-10 21:28:56 +0100

    add get port and ip client

--- PartRespond/cgi/clsCGI.hpp
+++ PartRespond/cgi/clsCGI.hpp
@@ -6,7 +6,7 @@
 /*   By: achamdao <achamdao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 14:39:09 by achamdao          #+#    #+#             */
-/*   Updated: 2026/05/10 16:04:02 by achamdao         ###   ########.fr       */
+/*   Updated: 2026/05/10 21:27:15 by achamdao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,6 +73,7 @@ class clsCGI
         const time_t &getStartTime() const;
         void Reset();
         void SetBuffer(char *Buffer);
+        void SetPortS_and_IPC(char *IPC, char PosrtS);
         ~clsCGI();
 };
 
